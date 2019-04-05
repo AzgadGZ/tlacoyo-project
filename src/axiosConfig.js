ff3c650e81e1c01c3d89ef857039000db7d4afa5
Mike <mortegabedol@dxc.com>
2019-04-05 11:39:18 -0600

    agregado el https

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://todo-api-tl.herokuapp.com/api/";
+axios.defaults.baseURL = "https://todo-api-tl.herokuapp.com/api/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.response.use(
